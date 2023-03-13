fbed14534a7f41a01f2ee160996af67f5e86882d
Eileen D'Angelo <eileenrkerekes@gmail.com>
2023-03-13 14:25:48 -0400

    add: assets add:projects.json

--- app/page.js
+++ app/page.js
@@ -1,10 +1,19 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import data from "../data/projects.json";
+
 
 export default function Home() {
+  const names = data.map((person) => {
+    return (
+      <img src={person.src} alt={person.name} />
+    )
+  });
+
   return (
     <main className={`${styles.main}`}>
+      <p>{names}</p>
       <h1>testing from the main page</h1>
     </main>
-  )
+  );
 }
