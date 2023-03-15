5dcb11599fde3bacff14a3102f6b08b695386e23
Eileen D'Angelo <eileenrkerekes@gmail.com>
2023-03-15 12:04:55 -0400

    finish: about header tablet CSS

--- app/about/page.js
+++ app/about/page.js
@@ -5,14 +5,15 @@ export default function () {
   const { width } = GetWindowWidth();
   return (
     <>
-      <header className={`${styles.header} relative max-w-[1110px] flex justify-center`}>
+      <header className={`${styles.header} relative max-w-[1110px] flex justify-center md:mx-[13%]`}>
         <div className={` ${styles.heroContainer}`}>
           <div className={`${styles.heroImage}`} ></div>
           <div className={`${styles.headerWhiteBox}`}></div>
-          <h1 className={`px-[8%] mb-[21px] ${styles.heading}`}>
+          <div className={`${styles.headerAboutTextContainer}`}><p className={`${styles.headerAboutText}`}>About</p> <div className={`${styles.headerHr}`}></div></div>
+          <h1 className={`px-[8%] md:px-0 mb-[21px] ${styles.heading}`}>
             Your team of professionals
           </h1>
-          <p className={`px-[8%] mb-[76px] ${styles.text}`}>
+          <p className={`px-[8%] mb-[76px] md:px-0 md:mb-0 ${styles.text}`}>
             Our small team of world-class professionals will work with you every
             step of the way. Strong relationships are at the core of everything
             we do. This extends to the relationship our projects have with their
