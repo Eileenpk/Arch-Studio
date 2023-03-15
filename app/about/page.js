c6af05ee85a02c9fa875f2a6cb14b47aaebf99f1
Eileen D'Angelo <eileenrkerekes@gmail.com>
2023-03-15 10:57:52 -0400

    fix: mobile nav position, finish: about mobile header content and CSS

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,33 @@
+"use client";
+import styles from "./about.module.css";
+import GetWindowWidth from "../functions/GetWindowWidth";
 export default function () {
-    return (
-        <h1>hello from about page!</h1>
-    )
+  const { width } = GetWindowWidth();
+  return (
+    <>
+      <header className={`${styles.header} relative max-w-[1110px] flex justify-center`}>
+        <div className={` ${styles.heroContainer}`}>
+          <div className={`${styles.heroImage}`} ></div>
+          <div className={`${styles.headerWhiteBox}`}></div>
+          <h1 className={`px-[8%] mb-[21px] ${styles.heading}`}>
+            Your team of professionals
+          </h1>
+          <p className={`px-[8%] mb-[76px] ${styles.text}`}>
+            Our small team of world-class professionals will work with you every
+            step of the way. Strong relationships are at the core of everything
+            we do. This extends to the relationship our projects have with their
+            surroundings.
+          </p>
+        </div>
+
+        <aside className={`absolute left-[38px] ${styles.aside}`}>
+          <div className={`${styles.asideBar}`}></div>
+          <h2 className={`${styles.asideText}`}>ABOUT US</h2>
+        </aside>
+      </header>
+      <main>
+        <h2>hello from about page!</h2>
+      </main>
+    </>
+  );
 }
