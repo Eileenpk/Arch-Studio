51c67fe5444c578a82dff60c62d111e78e73af5c
Eileen D'Angelo <eileenrkerekes@gmail.com>
2023-04-03 14:58:42 -0400

    feat: mobile, tablet, and desktop profile images mapped and sized

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -4,17 +4,35 @@ import styles from "./portfolio.module.css";
 import GetWindowWidth from '../functions/GetWindowWidth'
 export default function Portfolio() {
   const {width} = GetWindowWidth()
+  
   const names = data.map((project) => {
-    return <img src={project.mobileSrc} alt={project.name} className={styles.projectImg} />;
+    if(width < 768) {
+      return <img src={project.mobileSrc} alt={project.name} className= {styles.projectImg} />;
+    }
+
+    else if(width > 768 && width < 1280) {
+      return <img src={project.tabletSrc} alt={project.name} className={styles.projectImg} />;
+    }
+    else {
+      return <img src={project.desktopSrc} alt={project.name} className={styles.projectImg} />;
+    }
+    
   });
 
   return (
-    <main>
-      <div className="flex flex-wrap justify-center">{names}</div>
+    <>
+      <header className="relative">
       <aside className={`absolute left-[38px] ${styles.aside}`}>
           <div className={`${styles.asideBar}`}></div>
-          <h2 className={`${styles.asideText}`}>CONTACT</h2>
+          <h2 className={`${styles.asideText}`}>PORTFOLIO</h2>
         </aside>
+      </header>
+
+      <main className="max-w-[1110px]">
+      <div className="flex flex-wrap justify-center gap-6 xl:gap-8">{names}</div>
+      
     </main>
+    </>
+    
   );
 }
